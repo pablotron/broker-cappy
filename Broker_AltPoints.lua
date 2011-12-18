b6bbae908fea2ad4c0985403f632534b054ee78c
pabs <pabs@pablotron.org>
2011-12-18 17:23:55 -0500

    change name to cappy, add comments and clean up code a bit

--- Broker_AltPoints.lua
+++ Broker_AltPoints.lua
@@ -1,4 +1,4 @@
-local ADDON_NAME = "Broker_AltPoints"
+local ADDON_NAME = "Broker_Cappy"
 local LDB = LibStub:GetLibrary("LibDataBroker-1.1")
 local LibQTip = LibStub:GetLibrary('LibQTip-1.0')
 local tooltip
@@ -12,11 +12,11 @@ local CURRENCY_IDS = {
 }
 
 -- FIXME: make configurable
-local CURRENCY_LAYOUT = 'vertical'
+local CURRENCY_LAYOUT = 'v_all'
 
 -- layout for each character
 local CURRENCY_LAYOUTS = {
-  horizontal = {
+  h_all = {
     init = {
       'LEFT', 'RIGHT', 'RIGHT', 'RIGHT',
       'CENTER',
@@ -29,7 +29,7 @@ local CURRENCY_LAYOUTS = {
     }
   },
 
-  vertical = {
+  v_all = {
     init = {
       'LEFT', 'RIGHT', 'RIGHT', 'RIGHT',
     },
@@ -41,8 +41,55 @@ local CURRENCY_LAYOUTS = {
       {390},
     }
   },
+
+  h_pve = {
+    init = {
+      'LEFT', 'RIGHT', 'RIGHT', 'RIGHT',
+      'CENTER',
+      'LEFT', 'RIGHT', 'RIGHT', 'RIGHT'
+    },
+
+    grid = {
+      {395, 396},
+    }
+  },
+
+  v_pve = {
+    init = {
+      'LEFT', 'RIGHT', 'RIGHT', 'RIGHT',
+    },
+
+    grid = {
+      {395},
+      {396},
+    }
+  },
+
+  h_pvp = {
+    init = {
+      'LEFT', 'RIGHT', 'RIGHT', 'RIGHT',
+      'CENTER',
+      'LEFT', 'RIGHT', 'RIGHT', 'RIGHT'
+    },
+
+    grid = {
+      {392, 390},
+    }
+  },
+
+  v_pvp = {
+    init = {
+      'LEFT', 'RIGHT', 'RIGHT', 'RIGHT',
+    },
+
+    grid = {
+      {392},
+      {390},
+    }
+  },
 }
 
+-- class icons (not used atm)
 local CLASS_ICONS = {
   DEATHKNIGHT = "Interface/Icons/Spell_Deathknight_ClassIcon",
   DRUID       = "Interface/Icons/INV_Misc_MonsterClaw_04",
@@ -55,6 +102,10 @@ local CLASS_ICONS = {
   WARLOCK     = "Interface/Icons/Spell_Nature_FaerieFire",
   WARRIOR     = "Interface/Icons/INV_Sword_27",
 }
+
+--
+-- init/update methods
+--
 
 local function db_init(guid)
   -- lazily initialize database
@@ -123,6 +174,10 @@ local function update()
   end
 end
 
+--
+-- tooltip methods
+--
+
 local function normalize_cap(curr_id, val)
   if curr_id == 390 then
     return val
@@ -177,7 +232,7 @@ local function get_row(curr_id, curr)
       cap_val = curr.earnedThisWeek
       cap_max = normalize_cap(curr_id, curr.weeklyMax)
     elseif curr.totalMax and curr.totalMax > 0 then
-      cap_val = amount
+      cap_val = curr.amount
       cap_max = normalize_cap(curr_id, curr.totalMax)
     end
 
@@ -213,6 +268,90 @@ local function get_row(curr_id, curr)
   }
 end
 
+--
+-- data broker methods
+--
+
+local function sorted_accounts(t) 
+  local ids = {}
+
+  -- build unsorted list of ids
+  for k, _ in pairs(t) do 
+    table.insert(ids, k)
+  end
+
+  -- sort ids by name
+  table.sort(ids, function(a, b)
+    return t[a].name < t[b].name
+  end)
+
+  -- iterator variable
+  local i = 0
+
+  local iter = function()
+    -- increment iterator variable
+    i = i + 1
+
+    -- yield next value, or stop if we're at the end
+    if ids[i] == nil then
+      return nil
+    else
+      return ids[i], t[ids[i]]
+    end
+  end
+
+  -- return iterator
+  return iter
+end
+
+function add_char_to_tooltip(tooltip, guid, char, layout)
+  -- add character name
+  tooltip:AddLine(get_char_header(guid, char))
+
+  if char.currencies then
+    -- iterate over currency layout
+    for _, row_curr_ids in ipairs(layout.grid) do
+      local row = {};
+
+      -- chat_log('adding row')
+      -- chat_log('#row_curr_ids = ' .. #row_curr_ids)
+
+      for _, curr_id in ipairs(row_curr_ids) do
+        local curr = char.currencies[curr_id]
+
+        -- chat_log('curr_id = ' .. curr_id)
+
+        if curr then
+          -- add row
+          -- chat_log('add row')
+          for _, val in ipairs(get_row(curr_id, curr)) do
+            table.insert(row, val)
+          end
+        else
+          -- add empty set
+          -- chat_log('empty set')
+          for i = 1, 4 do
+            table.insert(row, ' ')
+          end
+        end
+
+        -- add empty cell for padding
+        table.insert(row, ' ')
+      end
+
+      -- remove trailing padding
+      row[#row] = nil
+
+      -- chat_log('adding combined row ')
+      -- add combined row
+      tooltip:AddLine(unpack(row))
+    end
+  end
+
+  -- delimit characters
+  tooltip:AddLine(' ')
+end
+
 local bac = LDB:NewDataObject(ADDON_NAME, {
   type = "data source",
   icon = "Interface/Icons/Inv_Misc_Armorkit_18",
@@ -227,6 +366,7 @@ end
 function bac.OnEnter(self)
   local layout = CURRENCY_LAYOUTS[CURRENCY_LAYOUT]
   local cols = layout.init
+  local curr_guid = UnitGUID('player')
 
   -- create tooltip
   tooltip = LibQTip:Acquire(ADDON_NAME .. 'Tooltip', #cols, unpack(cols))
@@ -236,53 +376,18 @@ function bac.OnEnter(self)
   tooltip:AddLine(' ')
 
   -- walk over characters
-  for guid, char in pairs(db) do
-    -- add character name
-    tooltip:AddLine(get_char_header(guid, char))
-
-    if char.currencies then
-      -- iterate over currency layout
-      for _, row_curr_ids in pairs(layout.grid) do
-        local row = {};
-
-        -- chat_log('adding row')
-        -- chat_log('#row_curr_ids = ' .. #row_curr_ids)
-
-        for _, curr_id in pairs(row_curr_ids) do
-          local curr = char.currencies[curr_id]
-
-          -- chat_log('curr_id = ' .. curr_id)
-
-          if curr then
-            -- add row
-            -- chat_log('add row')
-            for _, val in pairs(get_row(curr_id, curr)) do
-              table.insert(row, val)
-            end
-          else
-            -- add empty set
-            -- chat_log('empty set')
-            for i = 1, 4 do
-              table.insert(row, ' ')
-            end
-          end
-
-          -- add empty cell for padding
-          table.insert(row, ' ')
-        end
-
-        -- remove trailing padding
-        row[#row] = nil
-
-        -- chat_log('adding combined row ')
-        -- add combined row
-        tooltip:AddLine(unpack(row))
-      end
+  for guid, char in sorted_accounts(db) do
+    if guid ~= curr_guid then
+      add_char_to_tooltip(tooltip, guid, char, layout)
     end
-
-    -- delimit characters
-    tooltip:AddLine(' ')
   end
+
+  -- add separator
+  tooltip:AddSeparator()
+  tooltip:AddLine(' ')
+
+  -- add current character
+  add_char_to_tooltip(tooltip, curr_guid, db[curr_guid], layout)
 
   -- show tooltip
   tooltip:SmartAnchorTo(self)
@@ -294,11 +399,19 @@ function bac.OnLeave(self)
   tooltip = nil
 end
 
+--
+-- wow event methods
+--
+
 -- set up a local hidden frame to receive relevant currency events
 local frame = CreateFrame("frame")
+
+-- bind to events
 frame:RegisterEvent("PLAYER_ENTERING_WORLD")
 frame:RegisterEvent("CURRENCY_DISPLAY_UPDATE")
 frame:RegisterEvent("ADDON_LOADED")
+
+-- add event handler
 frame:SetScript("OnEvent", function(self, ev, ...) 
   -- print version information on load
   if ev == "PLAYER_ENTERING_WORLD" then
