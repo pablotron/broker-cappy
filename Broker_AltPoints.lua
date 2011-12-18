a5baf3e29da91cde1e551ae38bbf5f17376c6175
pabs <pabs@pablotron.org>
2011-12-18 01:44:53 -0500

    fix everything, make green empty and red capped (this might eed to be reversed

--- Broker_AltPoints.lua
+++ Broker_AltPoints.lua
@@ -1,5 +1,7 @@
 local ADDON_NAME = "Broker_AltPoints"
 local LDB = LibStub:GetLibrary("LibDataBroker-1.1")
+local LibQTip = LibStub:GetLibrary('LibQTip-1.0')
+local tooltip
 
 -- tracked currency ids
 local CURRENCY_IDS = {
@@ -21,7 +23,16 @@ local function db_init(guid)
   end
 
   -- handle first time load, name changes, faction changes, etc
-  db[guid]["name"] = GetUnitName('player')
+  db[guid].name = GetUnitName('player')
+
+  -- save class (for future color of player text)
+  local class, class_fn = UnitClass('player')
+  db[guid].class = class_fn
+
+  -- initialize currency table
+  if db[guid].currencies == nil then
+    db[guid].currencies = {}
+  end
 end
 
 local function chat_log(str)
@@ -56,7 +67,7 @@ local function update()
       -- then save the info
       if isDiscovered then
         -- save currency stats for this character/currency
-        db[guid][curr_id] = {
+        db[guid].currencies[curr_id] = {
           ["amount"]          = amount,
           ["earnedThisWeek"]  = earnedThisWeek,
           ["weeklyMax"]       = weeklyMax,
@@ -67,82 +78,85 @@ local function update()
   end
 end
 
-local function old_update()
-  local guid = UnitGUID('player')
-  local list_size = GetCurrencyListSize()
-
-  -- init database
-  db_init(guid)
-
-  -- print currency count (debug)
-  chat_log(string.format('guid = %s, list_size = %d', 
-    guid,
-    list_size
-  ))
-
-  -- walk over list of currencies
-  for i = 1, list_size do
-    local curr_name, is_header, is_expanded, is_unused, is_watched, count, extra_currency_type, icon, curr_id = GetCurrencyListInfo(i)
-
-    -- print currency name
-    chat_log(string.format("currency name = %s, is_header = %d",
-      curr_name,
-      is_header
-    ))
-
-    -- if this currency has a name and is not a header, 
-    -- then try and get info about it
-    if curr_name and not is_header then
-      -- get currency info for this character/currency
-      local name, amount, texture, earnedThisWeek, weeklyMax, totalMax, isDiscovered = GetCurrencyInfo(i)
-
-      -- dump currency id
-      -- chat_log("curr_id = " .. curr_id)
-
-      if name then
-        -- print currency name
-        chat_log("currency = " .. name)
-
-        -- if the character has ever discovered this currency,
-        -- then save the info
-        if isDiscovered then
-          -- save currency stats for this character/currency
-          db[guid][curr_id] = {
-            ["amount"]          = amount,
-            ["earnedThisWeek"]  = earnedThisWeek,
-            ["weeklyMax"]       = weeklyMax,
-            ["totalMax"]        = totalMax,
-          }
-        end
-      end
-    end
+local function normalize_cap(curr_id, val)
+  if curr_id == 390 then
+    return val
+  else
+    return val / 100
   end
 end
 
+local function get_char_name(char) 
+  local col_str = 'ffffff'
+
+  -- disable this for now
+  if false and char.class then
+    local c = RAID_CLASS_COLORS[char.class]
+    if c then
+      col_str = string.format('%02x%02x%02x', 
+        floor(255 * c.r),
+        floor(255 * c.g),
+        floor(255 * c.b)
+      )
+    end
+  end
+
+
+  -- TODO: add support for CLASS_ICON_TCOORDS
+  return string.format('|cff%s%s|r', col_str, char.name)
+end
+
 local function get_row(curr_id, curr) 
-  local cap = ''
-  local name, amount, texture, earnedThisWeek, weeklyMax, totalMax, isDiscovered = GetCurrencyInfo(curr_id)
+  local cap_str, cap_pct = 'n/a', 'n/a'
+  local name, amount, icon, earnedThisWeek, weeklyMax, totalMax, isDiscovered = GetCurrencyInfo(curr_id)
 
   -- check for weekly/total caps
-  -- note: WoW API returns the caps * 100 for some reason...
-  if curr.weeklyMax and curr.weeklyMax > 0 then
-    cap = string.format(" (%d/%d)", curr.earnedThisWeek, curr.weeklyMax / 100)
-  elseif curr.totalMax and curr.totalMax > 0 then
-    cap = string.format("/%d", curr.totalMax / 100)
+  -- note: WoW API returns the caps * 100 for some reason, except for
+  -- conquest points...
+  if (curr.weeklyMax > 0) or (curr.totalMax > 0) then
+    local cap_col, cap_val, cap_max = 'FFFFFF', 0, 0;
+
+    if curr.weeklyMax and curr.weeklyMax > 0 then
+      cap_val = curr.earnedThisWeek
+      cap_max = normalize_cap(curr_id, curr.weeklyMax)
+    elseif curr.totalMax and curr.totalMax > 0 then
+      cap_val = amount
+      cap_max = normalize_cap(curr_id, curr.totalMax)
+    end
+
+    -- calculate percent complete
+    cap_pct = 1.0 * cap_val / cap_max
+
+    -- TODO: make this a table?
+    if cap_pct > 1.0 then
+      cap_col = 'FF0000'
+    elseif cap_pct > 0.75 then
+      cap_col = 'FFA500'
+    elseif cap_pct > 0.5 then
+      cap_col = 'FFFF00'
+    elseif cap_pct > 0.25 then
+      cap_col = 'A5FF00'
+    else
+      cap_col = '00FF00'
+    end
+
+    -- build cap string
+    cap_str = string.format("|cFF%s%d/%d|r", cap_col, cap_val, cap_max)
+
+    -- convert percent to decimal and round
+    cap_pct = string.format('|cFF%s%d%%|r', cap_col, floor(100 * cap_pct + 0.5))
   end
 
   -- build/return formatted row
-  return string.format("\124TInterface/Icons/%s:0\124t |cFFFFFFFF%s|r %d%s", 
-    texture,
-    name,
-    curr.amount,
-    cap
-  )
+  return string.format("\124TInterface/Icons/%s:0\124t |cFFFFFFFF%s|r", icon, name),
+         curr.amount,
+         string.format("%s", cap_str),
+         cap_pct
 end
 
 local bac = LDB:NewDataObject(ADDON_NAME, {
   type = "data source",
-  icon = "Interface\\Icons\\Inv_Misc_Armorkit_18",
+  icon = "Interface/Icons/Inv_Misc_Armorkit_18",
   text = ""
 })
 
@@ -151,19 +165,25 @@ function bac.OnClick()
   ToggleCharacter("TokenFrame")
 end
 
-function bac.OnTooltipShow(tooltip)
-  -- add tooltip header
-  tooltip:AddLine(ADDON_NAME)
+function bac.OnEnter(self)
+  -- create tooltip
+  tooltip = LibQTip:Acquire(
+    ADDON_NAME .. 'Tooltip', 4, 
+    'LEFT', 'RIGHT', 'RIGHT', 'RIGHT'
+  )
+
+  -- add header
+  tooltip:AddHeader(ADDON_NAME)
   tooltip:AddLine(' ')
 
   -- walk over characters
-  for guid, currs in pairs(db) do
+  for guid, char in pairs(db) do
     -- add character name
-    tooltip:AddLine(currs.name)
+    tooltip:AddLine(get_char_name(char))
 
     -- iterate over currencies
-    for curr_id, curr in pairs(currs) do
-      if curr_id ~= 'name' then
+    if char.currencies then
+      for curr_id, curr in pairs(char.currencies) do
         tooltip:AddLine(get_row(curr_id, curr))
       end
     end
@@ -171,6 +191,15 @@ function bac.OnTooltipShow(tooltip)
     -- delimit characters
     tooltip:AddLine(' ')
   end
+
+  -- show tooltip
+  tooltip:SmartAnchorTo(self)
+  tooltip:Show()
+end
+
+function bac.OnLeave(self)
+  LibQTip:Release(tooltip)
+  tooltip = nil
 end
 
 -- set up a local hidden frame to receive relevant currency events
